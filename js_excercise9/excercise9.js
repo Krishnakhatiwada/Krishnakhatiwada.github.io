e7540b7da65414b769d43101ab6db3253411a1b2
kricna.khati <kricna.khati@gmail.com>
2020-06-24 01:57:24 +0545

    excercise 9 add

---
+++ js_excercise9/excercise9.js
@@ -0,0 +1,40 @@
+function creteWrapperDiv(circle) {
+  var speed = 3;
+  var div = document.createElement("div");
+  div.setAttribute("id", "bounce-wrapper");
+  div.style.height = "400px";
+  div.style.width = "400px";
+  div.style.position = "relative";
+  div.style.border = "2px solid black";
+  div.appendChild(circle);
+  var new_top = 0;
+  setInterval(function () {
+    if (new_top > 350) {
+      speed = -3;
+    }
+
+    if (new_top < 0) {
+      speed = +3;
+    }
+    new_top = new_top + speed;
+    circle.style.top = new_top + "px";
+  }, 800 / 60);
+  document.body.append(div);
+}
+
+function createBall() {
+  var ball = document.createElement("div");
+  ball.setAttribute("id", "ball");
+  ball.style.height = "50px";
+  ball.style.width = "50px";
+  ball.style.borderRadius = "30px";
+  ball.style.background = "red";
+  ball.style.position = "absolute";
+  ball.style.left = "150px";
+  ball.style.top = "0px";
+  return ball;
+  //   console.log(ball.top);
+}
+
+var balls = createBall();
+creteWrapperDiv(balls);
